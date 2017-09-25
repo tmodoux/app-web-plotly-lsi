61a9818af73a68b2fe125710c22cc3a33ad9616b
Thiébaud Modoux <tmodoux@pryv.com>
2017-09-25 14:40:10 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# app-web-plotly-lsi
+
+Basic webapp that allows to visualize Pryv data in real time using the Plotly graph library.
+
+This app loads (via Pryv monitors) any [numerical type](https://api.pryv.com/event-types/#numerical-types) Pryv data and plots it in a separate graph per stream/data-type.
+
+It also presents a special graph for voltammetry.
+
+The app is accessible at the following URL: [https://tmodoux.github.io/app-web-plotly/](https://pryv.github.io/app-web-plotly/)
---
+++ index.js
@@ -0,0 +1,417 @@
+/*globals document, pryv, _, Plotly*/
+var container = document.getElementById('pryvGraphs');
+var monitor;
+
+
+/**
+ * retrieve the registerURL from URL parameters
+ */
+function getRegisterURL() {
+  return pryv.utility.urls.parseClientURL().parseQuery()['reg-pryv'] || pryv.utility.urls.parseClientURL().parseQuery()['pryv-reg'];
+}
+
+var customRegisterUrl = getRegisterURL();
+if (customRegisterUrl) {
+  pryv.Auth.config.registerURL = {host: customRegisterUrl, 'ssl': true};
+}
+
+/**
+ * retrieve the registerURL from URL parameters
+ */
+function getSettingsFromURL() {
+  var settings = {
+    username : pryv.utility.urls.parseClientURL().parseQuery().username,
+    domain : pryv.utility.urls.parseClientURL().parseQuery().domain,
+    auth: pryv.utility.urls.parseClientURL().parseQuery().auth
+  };
+
+  if (settings.username && settings.auth) {
+    return settings;
+  }
+
+  return null;
+}
+
+function setupShareLink(connect) {
+  var urlLabel = document.getElementById('sharelink');
+  urlLabel.innerHTML = ('' + document.location).split('?')[0] +
+    '?username=' + connect.username +
+    '&domain=' + connect.domain +
+    '&auth=' + connect.auth;
+}
+
+document.onreadystatechange = function () {
+
+  document.getElementById('loading').style.display = 'none';
+  document.getElementById('logo-pryv').style.display = 'block';
+  var state = document.readyState;
+  if (state == 'complete') {
+    var settings = getSettingsFromURL();
+    if (settings) {
+      var connection = new pryv.Connection(settings);
+      connection.fetchStructure(function () {
+        setupMonitor(connection);
+      });
+    } else {
+
+      // Authenticate user
+      var authSettings = {
+        requestingAppId: 'appweb-plotly',
+        requestedPermissions: [
+          {
+            streamId: '*',
+            level: 'read'
+          }
+        ],
+        returnURL: false,
+        spanButtonID: 'pryv-button',
+        callbacks: {
+          needSignin: resetPlots,
+          needValidation: null,
+          signedIn: function (connect) {
+            plotVoltammetry(connect);
+            connect.fetchStructure(function () {
+              setupMonitor(connect);
+            });
+          }
+        }
+      };
+      pryv.Auth.setup(authSettings);
+    }
+  }
+};
+
+// MONITORING
+// Setup monitoring for remote changes
+function setupMonitor(connection) {
+  setupShareLink(connection);
+
+  document.getElementById('loading').style.display = 'block';
+  document.getElementById('logo-pryv').style.display = 'none';
+  var filter = new pryv.Filter({limit: 10000});
+  monitor = connection.monitor(filter);
+
+  // should be false by default, will be updated in next lib version
+  // to use fullCache call connection.ensureStructureFetched before
+  monitor.ensureFullCache = false;
+  monitor.initWithPrefetch = 0; // default = 100;
+
+  // get presets from stream structure
+  connection.streams.walkTree({}, function (stream) { 
+    if (stream.clientData && stream.clientData['app-web-plotly']) {
+      Object.keys(stream.clientData['app-web-plotly']).forEach(function(eventType) {
+        var traceKey = stream.id + '_' + eventType;
+        presets[traceKey] = stream.clientData['app-web-plotly'][eventType];
+      });
+    }
+    console.log('Stream:' + stream.id + '->' + JSON.stringify(stream.clientData));
+  });
+
+
+  // get notified when monitoring starts
+  monitor.addEventListener(pryv.MESSAGES.MONITOR.ON_LOAD, function (events) {
+
+    document.getElementById('loading').style.display = 'none';
+    document.getElementById('logo-pryv').style.display = 'block';
+    updatePlot(events);
+
+  });
+
+  // get notified when data changes
+  monitor.addEventListener(pryv.MESSAGES.MONITOR.ON_EVENT_CHANGE, function (changes) {
+    updatePlot(changes.created);
+  });
+
+  // start monitoring
+  monitor.start(function (/**err**/) {
+  });
+}
+
+// Traces
+var traces = {};
+var presets = {};
+var plots = {};
+
+function getDateString(timestamp) {
+  var date = new Date(timestamp);
+  return date.toISOString().substring(0, 10) + ' '  +
+    date.toISOString().substring(11, 19) + '.' + date.getMilliseconds();
+}
+
+function createTrace(event) {
+  var traceKey = event.streamId + '_' + event.type;
+  var extraType = pryv.eventTypes.extras(event.type);
+  var titleY = extraType.symbol ? extraType.symbol : event.type;
+
+  if(presets[traceKey] && presets[traceKey].titleY) {
+    titleY = presets[traceKey].titleY;
+  }
+
+  traces[traceKey] = {
+    plotKey: traceKey,
+    type: event.type,
+    streamId: event.streamId + ' ' + titleY,
+    last: event.timeLT,
+    gaps: null,
+    trace: {},
+    yaxis : {
+      yaxis1: {
+        title : titleY,
+        showticklabels : true,
+        side: 'right'
+      }
+    }
+  };
+
+  if (presets[traceKey]) {
+    _.extend(traces[traceKey], presets[traceKey]);
+  }
+
+  traces[traceKey].trace.x = [];
+  traces[traceKey].trace.y = [];
+
+  if (! plots[traces[traceKey].plotKey]) {
+    var title = '';
+
+    if (presets[traceKey] && presets[traceKey].plotKey) {
+      // name per plotKey
+      title = presets[traceKey].plotKey;
+    } else { // take stream path
+      event.stream.ancestors.forEach(function (ancestor) {
+        title += ancestor.name + '/';
+      });
+      title += event.stream.name;
+    }
+    plots[traces[traceKey].plotKey] = {
+      layout : { title : title }
+    };
+  }
+
+  plots[traces[traceKey].plotKey].layout.xaxis = {
+    rangeselector: selectorOptions,
+    title: 'Time',
+    type: 'date',
+    showticklabels : true
+  };
+
+  if (! plots[traces[traceKey].plotKey].num) { // first plot
+    plots[traces[traceKey].plotKey].num = 1;
+    traces[traceKey].layout = {
+      yaxis1: {
+        title : titleY,
+        showticklabels : true,
+        side: 'left'
+      }
+    };
+
+  } else {
+    var num = ++plots[traces[traceKey].plotKey].num;
+    var pos = 1 - + ((num-2) * 0.05);
+    traces[traceKey].layout = {};
+    traces[traceKey].layout['yaxis' + num] = {
+      title : titleY,
+      showticklabels : true,
+      side: 'right',
+      overlaying: 'y',
+      position: pos
+    };
+    traces[traceKey].trace.yaxis = 'y' + num;
+  }
+}
+
+var initializedTraces = {};
+var initializedPlots = {};
+
+var lastLastX = 0;
+var gap = 1 * 30 * 1000;
+
+function initOrRedraw(traceKey) {
+
+  var trace = traces[traceKey];
+  if (initializedTraces[traceKey]) {
+    if (liveRange && (lastX  > (lastLastX + gap))) {
+      var start = lastX - liveRange * 60 * 1000;
+      var stop = lastX + 1 * 30 * 1000;
+      lastLastX = lastX;
+      setAllRanges(getDateString(start), getDateString(stop));
+      previousWasLiverange = true;
+    }
+
+    return Plotly.redraw(trace.plotKey);
+  }
+  initializedTraces[traceKey] = true;
+
+  if (! initializedPlots[trace.plotKey]) {
+    initializedPlots[trace.plotKey] = true;
+    var plot = document.createElement('div');
+    plot.setAttribute('id', trace.plotKey);
+    container.appendChild(plot);
+
+    Plotly.newPlot(trace.plotKey, [], plots[trace.plotKey].layout);
+  }
+
+  Plotly.relayout(trace.plotKey, trace.layout);
+  Plotly.addTraces(trace.plotKey, [trace.trace]);
+}
+
+
+/**
+ * retrieve the registerURL from URL parameters
+ */
+function getLiveRangeURL() {
+  return pryv.utility.urls.parseClientURL().parseQuery()['liverange'];
+}
+
+
+var lastX = 0;
+var liveRange = getLiveRangeURL() || 0;
+
+
+
+var ignoreFrom = 0; // ((new Date().getTime())) - (60 * 60 * 24 * 1000 * 10);
+
+function updatePlot(events) {
+  // Needed ?
+  events = events.sort(function (a, b) {
+    return a.time - b.time;
+  });
+
+  var toRedraw = {};
+
+  events.map(function (event) {
+    var traceKey = event.streamId + '_' + event.type;
+
+    if (! pryv.eventTypes.isNumerical(event)) {
+      traces[traceKey] = { ignore : true};
+      return;
+    }
+
+    if (event.trashed || (ignoreFrom > event.timeLT)) {
+    return;
+    }
+
+    if (! traces[traceKey]) {
+      createTrace(event);
+    }
+
+    if (! traces[traceKey].ignore) {
+      if (traces[traceKey].gaps) {
+        if ((event.timeLT - traces[traceKey].last) > traces[traceKey].gaps * 1000) {
+          traces[traceKey].trace.x.push(getDateString(traces[traceKey].last + 1));
+          traces[traceKey].trace.y.push(null);
+        }
+      }
+
+      if (event.timeLT > lastX) {
+        lastX = event.timeLT;
+      }
+
+      traces[traceKey].trace.x.push(getDateString(event.timeLT));
+      traces[traceKey].trace.y.push(event.content);
+      traces[traceKey].last = event.timeLT;
+
+      toRedraw[traceKey] = true;
+    }
+
+  });
+
+  Object.keys(toRedraw).forEach(function (traceKey) {
+    initOrRedraw(traceKey);
+  });
+}
+
+function setAllForRealTime () {
+  var now = new Date().getTime();
+  var start = now - 5 * 60 * 1000;
+  var stop = now + 5 * 60 * 1000;
+  setAllRanges(getDateString(start), getDateString(stop));
+}
+
+function setAllRanges(start, stop) {
+  Object.keys(plots).forEach(function (plotKey) {
+    Plotly.relayout(plotKey, {xaxis: {range : [start, stop]}});
+  });
+}
+
+
+function resetPlots() {
+  if (monitor) {
+    monitor.destroy();
+  }
+}
+
+
+// *** Plotly designs ***  //
+var selectorOptions = {
+  buttons: [
+    {
+      step: 'hour',
+      stepmode: 'backward',
+      count: 1,
+      label: '1h'
+    }, {
+      step: 'day',
+      stepmode: 'backward',
+      count: 1,
+      label: '1d'
+    }, {
+    step: 'month',
+      stepmode: 'backward',
+      count: 1,
+      label: '1m'
+     }, {
+    step: 'month',
+    stepmode: 'backward',
+    count: 6,
+    label: '6m'
+  }, {
+    step: 'year',
+    stepmode: 'backward',
+    count: 1,
+    label: '1y'
+  }, {
+    step: 'all'
+  }]
+};
+
+function plotVoltammetry(connection) {
+  var plot = document.createElement('div');
+  plot.setAttribute('id', 'voltammetry');
+  container.appendChild(plot);
+  var filter = new pryv.Filter({streams : [plot.id]});
+  connection.events.get(filter, function (err, events) {
+    var voltage = [];
+    var current = [];
+    events.forEach(function(event) {
+      var jsonContent = JSON.parse(event.content);
+      voltage.push(jsonContent.voltage);
+      current.push(jsonContent.current);
+      console.log(jsonContent.voltage, jsonContent.current);
+    });
+    
+    var trace = {
+      x: voltage,
+      y: current,
+      mode: "lines",
+      name: "Voltammetry",
+      type: "scatter",
+      xaxis: "x1",
+      yaxis: "y1"
+    };
+    var layout = {
+      title: "Cyclic Voltammetry",
+      xaxis1: {
+          anchor: "y1",
+          domain: [0.0, 1.0],
+          title: "Potential (Volts)"
+      },
+      yaxis1: {
+          anchor: "x1",
+          domain: [0.0, 1.0],
+          title: "Current (uA)"
+      }
+    };
+    
+    Plotly.newPlot(plot.id, [trace], layout);
+  });
+}
